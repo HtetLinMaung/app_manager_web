5a35ce7832af0fe3b9e007e3db13b9f6cc45985e
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2023-03-03 04:42:54 +0630

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: "/app-manager",
+  rewrites() {
+    return [
+      {
+        source: "/app-manager/_next/:path*",
+        destination: "/_next/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
