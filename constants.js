203dade2a366b74a38101dc522e5ebfa7b31f53e
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2023-03-03 09:07:55 +0630

    update

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
-export const domain = "http://150.95.82.125:7070/app-manager/api";
-export const socketio_domain = "http://150.95.82.125:7070";
+// export const domain = "http://150.95.82.125:7070/app-manager/api";
+// export const socketio_domain = "http://150.95.82.125:7070";
 
-// export const domain = "http://20.53.229.133:7070/app-manager/api";
-// export const socketio_domain = "http://20.53.229.133:7070";
+export const domain = "http://20.248.191.100:7070/app-manager/api";
+export const socketio_domain = "http://20.248.191.100:7070";
 
 // export const domain = "http://4.197.17.178:7070/app-manager/api";
 // export const socketio_domain = "http://4.197.17.178:7070";
